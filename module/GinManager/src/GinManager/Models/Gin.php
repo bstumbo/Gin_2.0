7c8c46ee79b602f8ae7138988a12bd451b97bfef
Brandon Stumbo <bstumbo@gwu.edu>
2015-07-26 19:59:33 -0400

    Setup directory structure, module, module.config and start Gin class.
    Signed-off-by: Brandon Stumbo <bstumbo@gwu.edu>

---
+++ module/GinManager/src/GinManager/Models/Gin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace GinManager\Models;
+
+class Gin {
+    
+    public $ginId;
+    public $ginType;
+    public $ginImage;
+    public $ginThumb;
+    public $juniper;
+    public $citrus;
+    public $spice;
+    public $herbal;
+    public $floral;
+    public $proof;
+    public $aged;
+    public $tree;
+    public $ginPriceOz;
+    public $gindescription;
+    public $ginOrigin;
+    
+    
+    
+}
